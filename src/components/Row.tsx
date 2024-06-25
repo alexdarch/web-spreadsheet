78773b0a6bba49a8b2eb28575f16c30c0c216f3e
alexdarch <alex.darch@btinternet.com>
2024-06-25 08:46:19 +0100

    reverting eslint rubbish

---
+++ src/components/Row.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Cell from './Cell'
+
+export default function Row() {
+    return (
+        <table>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            <tr>
+                <Cell contents={'Alfreds Futterkiste2'} />
+                <td>Maria Anders</td>
+                <td>Germany</td>
+            </tr>
+            <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+            </tr>
+        </table>
+    )
+}
---
+++ src/components/Spreadsheet.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Row from './Row'
+
+export default function Spreadsheet() {
+    return (
+        <>
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+        </>
+    )
+}
