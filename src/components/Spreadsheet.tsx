3d614313a56d5b2d0549f3894f04c1637026f91c
alexdarch <alex.darch@btinternet.com>
2024-06-25 07:14:20 +0100

    adding eslint and prettier

---
+++ src/components/Spreadsheet.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cell from './Cell';
+
+export default function Spreadsheet() {
+  return (
+    <>
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+    </>
+  );
+}
